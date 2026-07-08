80abc8d15c842c5586464d0da6ea13b763f2565f
Ali Emre Gözden <aliemregozden@gmail.com>
2026-07-08 13:15:07 +0300

    Ders2: Değişkenler ve tipler

---
+++ src/ders2/d122_ValVarMantigi.kt
@@ -0,0 +1,18 @@
+package ders2
+
+fun main() {
+    //
+    val uygulamaAdi = "Hava durumu Pro"
+    //Val olan bir değişkene yeniden bir değer atanamaz
+
+    //
+    var indirimOrani = 10
+    indirimOrani = 15
+    //Var türünde olan değişkenin değeri değiştirilebilir
+
+    println("Uygulama adı: $uygulamaAdi")
+    println("İndirim Oranı: $indirimOrani")
+
+
+
+}
---
+++ src/ders2/d123_TipCikarimi.kt
@@ -0,0 +1,17 @@
+package ders2
+
+fun main() {
+    val sehir: String = "Ankara" //Değişken türü belirtilebilir ama gereksizdir
+    val sicaklik = 12
+    val nemOrani = 0.43
+    val dogrumu = true
+
+    /*
+    Eğer boş değişken oluşurulacaksa : koyup
+    değişken türü belirtilmelidir
+     */
+    var sistemMesaji: String
+    sistemMesaji = "sistem mağduru"
+
+    println("mesaj: $sistemMesaji")
+}
